523521915f6163bfe0ff9a1a0eff920e05a8648d
DanielNuud <daaniel.nuud@gmail.com>
2023-09-06 11:50:58 +0300

    adding Cli.java. Created method hello()

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,11 @@
+package hexlet.code;
+import java.util.Scanner;
+
+public class Cli {
+
+    public static void hello(){
+        System.out.print("May I have your name? ");
+        String inputName = new Scanner(System.in).nextLine();
+        System.out.println("Hello, "  + inputName + "!");
+    }
+}
