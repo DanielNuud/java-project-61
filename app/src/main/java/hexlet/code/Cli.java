eb996e9407541582b621a5576bdc7d6a02ac03e2
DanielNuud <daaniel.nuud@gmail.com>
2023-09-06 21:17:37 +0300

    fix by checkstyle

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -2,8 +2,7 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-
-    public static void hello(){
+    public static void hello() {
         System.out.print("May I have your name? ");
         String inputName = new Scanner(System.in).nextLine();
         System.out.println("Hello, "  + inputName + "!");
