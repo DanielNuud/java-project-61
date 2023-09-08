f8806837b78c95979bae27a4a4890ab5db31a85c
DanielNuud <daaniel.nuud@gmail.com>
2023-09-08 12:57:06 +0300

    fix with CheckStyle

--- app/src/main/java/hexlet/code/EvenGame.java
+++ app/src/main/java/hexlet/code/EvenGame.java
@@ -14,25 +14,30 @@ public class EvenGame {
             String userInput = new Scanner(System.in).nextLine();
 
             String correctAnswer = "";
-            if (randomNumber % 2 == 0){
+            if (randomNumber % 2 == 0) {
                 correctAnswer = "yes";
-            } else
+            } else {
                 correctAnswer = "no";
+            }
 
-            if ((randomNumber % 2 == 0 && userInput.equals("yes")) || (randomNumber % 2 != 0 && userInput.equals("no"))) {
+            if ((randomNumber % 2 == 0 && userInput.equals("yes"))
+                    || (randomNumber % 2 != 0 && userInput.equals("no"))) {
                 System.out.println("Correct!");
                 correctAnswers++;
-            } else if ((randomNumber % 2 == 0 && userInput.equals("no")) || (randomNumber % 2 != 0 && userInput.equals("yes")) ){
-                System.out.println("'" + userInput + "' is wrong answer ;(. Correct answer was '" + correctAnswer + "'.\n" +
-                        "Let's try again, " + username + "!");
+            } else if ((randomNumber % 2 == 0 && userInput.equals("no"))
+                    || (randomNumber % 2 != 0 && userInput.equals("yes"))) {
+                System.out.println("'" + userInput + "' is wrong answer ;(. "
+                        + "Correct answer was '" + correctAnswer + "'.\n"
+                        + "Let's try again, " + username + "!");
                 break;
             } else {
-                System.out.println("'" + userInput + "' is wrong answer ;(. Correct answer was '" + correctAnswer + "'");
+                System.out.println("'" + userInput + "' is wrong answer ;(. "
+                        + "Correct answer was '" + correctAnswer + "'");
                 break;
             }
         }
 
-        if (correctAnswers == 3){
+        if (correctAnswers == 3) {
             System.out.println("Congratulations, " + username + "!");
         }
 
