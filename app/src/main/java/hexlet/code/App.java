9e7de61fbb931ceaadcf74330a8d09511f7f2f7f
DanielNuud <daaniel.nuud@gmail.com>
2023-09-08 20:07:05 +0300

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,12 +1,15 @@
 package hexlet.code;
 
+import hexlet.code.games.CalcGame;
+import hexlet.code.games.EvenGame;
+
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
 
         System.out.println("Please enter the game number and press Enter.");
-        String[] gamesArray = {"Greet", "Even", "Exit"};
+        String[] gamesArray = {"Greet", "Even", "Calc", "Exit"};
         for (int i = 0; i < gamesArray.length; i++) {
             if (i != gamesArray.length - 1) {
                 System.out.println(i + 1 + " - " + gamesArray[i]);
@@ -18,13 +21,17 @@ public class App {
         int inputGameChoice = new Scanner(System.in).nextInt();
 
         System.out.println("Welcome to the Brain Games!");
+        String inputName = Cli.hello();
         switch (inputGameChoice) {
-            case 1: Cli.hello();
+            case 1:
                 break;
             case 2:
-                String inputName = Cli.hello();
-                EvenGame.evenCheck(inputName);
+                EvenGame.evenDesc();
+                Engine.game(inputGameChoice, inputName);
                 break;
+            case 3:
+                CalcGame.calcDesc();
+                Engine.game(inputGameChoice, inputName);
             case 0:
                 break;
             default:
