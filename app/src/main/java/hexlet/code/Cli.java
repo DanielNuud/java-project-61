69b312e306b17d8b4414dae661532dec926d822e
DanielNuud <daaniel.nuud@gmail.com>
2023-09-08 12:45:33 +0300

    fix method hello()

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -2,9 +2,10 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static void hello() {
+    public static String hello() {
         System.out.print("May I have your name? ");
         String inputName = new Scanner(System.in).nextLine();
-        System.out.println("Hello, "  + inputName + "!");
+        System.out.println("Hello, " + inputName + "!");
+        return inputName;
     }
 }
