2c30da4339615f8fa597a109058fd2e2e12f9420
DanielNuud <daaniel.nuud@gmail.com>
2023-10-02 16:31:59 +0300

    fix PrimeGame

--- app/src/main/java/hexlet/code/games/PrimeGame.java
+++ app/src/main/java/hexlet/code/games/PrimeGame.java
@@ -23,8 +23,9 @@ public class PrimeGame {
     }
 
     public static boolean isPrime(int number) {
+
         if (number <= 1) {
-            return true;
+            return false;
         }
 
         for (int i = 2; i <= Math.sqrt(number); i++) {
