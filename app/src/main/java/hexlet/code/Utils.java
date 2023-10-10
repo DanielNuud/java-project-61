0d0c734f976b1e3bbf7fdeee2be1494267e8b22f
DanielNuud <daaniel.nuud@gmail.com>
2023-10-10 14:26:16 +0300

    add Utils.java

---
+++ app/src/main/java/hexlet/code/Utils.java
@@ -0,0 +1,13 @@
+package hexlet.code;
+
+public class Utils {
+
+    public static int getRandomNumber(int max) {
+        return (int) (Math.random() * max);
+    }
+
+    public static int getRandomNumber(int min, int max) {
+        return (int) (Math.random() * (max - min) + min);
+    }
+}
+
